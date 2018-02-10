82ec31b5d86aaf5547405ba2379f9968fef59713
Peterhaiker <vim.memory@gmail.com>
2018-02-10 20:49:30 +0800

    init完成部分，守护进程完成检测无副本运行

--- Server/Func_def/init.c
+++ Server/Func_def/init.c
@@ -8,19 +8,52 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
 #include"../Header/server.h"
 
 void init(void)
 {
   daemon();
+  //ensure only one daemon running in background
+  int fd;
+  if(-1==(fd=open("/var/run/FTF.lock",O_CREAT|O_RDRW,0666))){
+    syslog(LOG_EMEGE,"create /var/run/FTF.lock failed:%m");
+    exit(EXIT_FAILURE);
+  }
+  struct flock flock;
+  flock.l_type=F_WRLCK;
+  flock.l_whence=SEEK_SET;
+  flock.l_start=0;
+  flock.l_len=0;
+  if(-1==fcntl(fd,F_SETLK,&flock)){
+    syslog(LOG_EMEGE,"FTF has runned in background:%m");
+    exit(EXIT_FAILURE);
+  }
+  write(fd,"%d",getpid());
+
   //connect to mysql
   mysql_init(&mysql);
   if(!mysql_real_connect(&mysql,SQL_ADDR,SQL_USER,SQL_PASS,SQL_DB,0,NULL,CLIENT_MULTI_STATEMENT)){
-    syslog(LOG_EMEGE,"init function:connect to mysql failed");
+    syslog(LOG_EMEGE,"init function:connect to mysql failed %m");
     exit(EXIT_FAILURE);
   }
   mysql_set_character_set(&mysql,"utf8");
   //create passwd table
-  if(mysql_query(&mysql,"create table passwd(id int(8) not null primary key auto_increment,account varchar(20) not null,nickname varchar(20) not null)"))
+  if(mysql_query(&mysql,"create table passwd(account mediumint not null primary key,nickname varchar(20),password varchar(50) not null)")){
+    //create failed
+    syslog(LOG_EMEGE,"create table passwd failed:%m");
+    exit(EXIT_FAILURE);
+  }
+  //create profile table
+  if(mysql_query(&mysql,"create table profile(account mediumint not null,name varchar(20) default null,sex varchar(1),fri_nums tinyint,grp_nums tinyint,ip_addr int not null,ip_port smallint not null,primary key(account),key pro_ip_addr_ind(ip_addr),key pro_id_port_ind(ip_port),constraint fk_account foreign key(account) references passwd(account))")){
+    syslog(LOG_EMEGE,"create table passwd failed:%m");
+    exit(EXIT_FAILURE);
+  }
+  //create friends table
+  if(mysql_query(&mysql,"create table friends(account mediumint not null,ip_addr int not null,ip_port smallint not null,primary key(account),key fri_ip_addr_fk(ip_addr),key fri_ip_port_fk(ip_port),constraint fk_fri_account foreign key(account) references passwd(account),constraint fri_ip_addr_fk foreign key(ip_addr) references profile(ip_addr),constraint fri_ip_port_fk foreign key(ip_port) references profile(ip_port))")){
+    syslog(LOG_EMEGE,"create table friends failed:%m");
+    exit(EXIT_FAILURE);
+  }
   return;
 }
